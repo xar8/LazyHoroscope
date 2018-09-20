2fd465673ae1cbbb180c0a9fd490b83f591ad648
xar8 <jevans42@ecc-jaguar.csuchico.edu>
2018-09-19 17:09:43 -0700

    Ask for First and Last Name

--- main.cpp
+++ main.cpp
@@ -7,8 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
+  string firstname;
+  string lastname; 
 
   //get user input
+  cout<<"What is your first name?";
+  cin>>firstname;
+  cout<<"What is your last name?";
+  cin>>lastname;
 
   //tell fortune
 
